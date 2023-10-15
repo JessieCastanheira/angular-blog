94d48398d39f81106d9ad151150caff06dfe117f
JessieCastanheira <jessicab_castanheira@hotmail.com>
2023-10-15 02:03:58 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia novo homem de ferro",
+    "photoCover": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F02%2Firon-man-3-review-2000.jpg&q=60"
+  },
+  {
+    "id": "2",
+    "title": "Nova Série anunciada no DisneyPlus",
+    "description": "Eita",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
